1228a1774ca596224443b5f57c019b26bad9c645
melloom <Melvin.a.p.cruz@gmail.com>
2026-03-28 23:22:15 -0400

    Update CSP to allow Vercel Analytics and Google Tag Manager scripts

--- next.config.mjs
+++ next.config.mjs
@@ -20,11 +20,11 @@ const nextConfig = {
             key: 'Content-Security-Policy',
             value: [
               "default-src 'self'",
-              "script-src 'self' 'unsafe-eval' 'unsafe-inline' https://apis.google.com https://*.firebaseio.com https://*.googleapis.com",
+              "script-src 'self' 'unsafe-eval' 'unsafe-inline' https://apis.google.com https://*.firebaseio.com https://*.googleapis.com https://va.vercel-scripts.com https://www.googletagmanager.com",
               "style-src 'self' 'unsafe-inline' https://fonts.googleapis.com",
               "font-src 'self' https://fonts.gstatic.com",
               "img-src 'self' data: blob: https://*.googleapis.com https://*.gstatic.com https://source.unsplash.com https://images.unsplash.com https://*.ggpht.com",
-              "connect-src 'self' https://*.googleapis.com https://*.google.com https://*.firebaseio.com https://*.firebaseapp.com wss://*.firebaseio.com https://overpass-api.de https://api.foursquare.com https://api.yelp.com https://*.firebase.googleapis.com",
+              "connect-src 'self' https://*.googleapis.com https://*.google.com https://*.firebaseio.com https://*.firebaseapp.com wss://*.firebaseio.com https://overpass-api.de https://api.foursquare.com https://api.yelp.com https://*.firebase.googleapis.com https://va.vercel-scripts.com https://www.googletagmanager.com",
               "frame-src 'self' https://*.firebaseapp.com https://accounts.google.com",
               "object-src 'none'",
               "base-uri 'self'",
